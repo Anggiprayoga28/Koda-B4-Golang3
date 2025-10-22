61ef675030f9f3cf53bcf5693ee967652592fd86
Anggiprayoga28 <anggiprayoga930@gmail.com>
2025-10-22 11:23:52 +0700

    Feat: Add defer and exit to display completed message and search result condition is empty or found

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"Golang3/data"
 	"fmt"
+	"os"
 )
 
 func main() {
+	defer fmt.Println("\nSelesai")
+
 	users := []data.Person{
 		{Name: "John Doe"},
 		{Name: "Jane Smith"},
@@ -21,4 +24,12 @@ func main() {
 	results := data.SearchPerson(&users, &keyword)
 
 	fmt.Println(results)
+
+	if len(results) == 0 {
+		fmt.Println("Tidak ada hasil ditemukan.")
+		os.Exit(0)
+	} else {
+		fmt.Printf("Ditemukan %d hasil.\n", len(results))
+		os.Exit(0)
+	}
 }
